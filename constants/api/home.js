2a79ae159e304c7484cd5ad8299b6904f819c05f
hehuangz <1025005090@qq.com>
2018-09-17 15:21:15 +0800

    20190917

--- constants/api/home.js
+++ constants/api/home.js
@@ -2,6 +2,7 @@ const api = {
     HOME_NEARLY_SHOP: 'shop/getNearlyShop',
     HOME_SAOMA: 'shop/getNearlyShop',
     HOME_ADVISER: 'users/searchAssistantByShopId',
+    HOME_ADVISER_USER: 'users/queryBindAssistant',
     HOME_CATEGORY_FIRST: 'goods/getFirstCategoryByShopId', // 根据店铺id获取一级分类
     HOME_COODS: 'goods/getGoodsByShopAndFirstId',
     HOME_SHOP_ADDRESS: 'shop/view'
